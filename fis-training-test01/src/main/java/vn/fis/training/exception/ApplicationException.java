2b05e66391b0f31909993603dd2c528d71d4ad6c
luocplay2001 <74364366+luocplay2001@users.noreply.github.com>
2022-06-04 19:18:04 +0700

    Add files via upload

---
+++ fis-training-test01/src/main/java/vn/fis/training/exception/ApplicationException.java
@@ -0,0 +1,17 @@
+package vn.fis.training.exception;
+
+public abstract class ApplicationException extends RuntimeException{
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException() {
+
+    }
+
+    /**
+     * @return: Ma loi duoc khai bao trong @vn.fis.training.service.AppConstant
+     */
+    public abstract String getErrorCode();
+}
