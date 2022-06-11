7c5272e1171c9c20d4737927cdf394b792fc6154
luocplay2001 <74364366+luocplay2001@users.noreply.github.com>
2022-06-11 20:56:35 +0700

    update

--- Spring_02_JDBC/src/main/java/fis/sprint02/dao/IDAO.java
+++ Spring_02_JDBC/src/main/java/fis/sprint02/dao/IDAO.java
@@ -1,4 +1,19 @@
 package fis.sprint02.dao;
 
-public interface IDAO {
+import java.util.List;
+import java.util.Optional;
+
+public interface IDAO<T> {
+    //CREATE
+    T save(T t);
+
+    //READ | RETRIEVE
+    Optional<T> get(long id);
+    List<T> getAll();
+
+    //UPDATE
+    T update(T t);
+
+    //DELETE
+    T delete(T t);
 }
