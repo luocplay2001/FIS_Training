7c8c11afd366500a3b6679692dca79f570ca3e26
luocplay2001 <74364366+luocplay2001@users.noreply.github.com>
2022-06-11 15:49:51 +0700

    update

--- Spring_02_JDBC/src/main/java/fis/sprint02/dao/jdbc/DBConnect.java
+++ Spring_02_JDBC/src/main/java/fis/sprint02/dao/jdbc/DBConnect.java
@@ -1,4 +1,17 @@
 package fis.sprint02.dao.jdbc;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+
 public class DBConnect {
+    public static Connection getConnection() throws Exception {
+        Connection con = ds.getConnection();
+        return con;
+    }
+
+    private static HikariDataSource ds;
+    private static HikariConfig config = new HikariConfig("/datasource.properties");
+
 }
