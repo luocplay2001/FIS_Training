506a05f2c8c171a04d1843c829d1dbeb0edf1d25
luocplay2001 <74364366+luocplay2001@users.noreply.github.com>
2022-06-28 08:32:40 +0700

    update

---
+++ final-test/src/main/java/vn/fis/finaltest/FinalTestApplication.java
@@ -0,0 +1,13 @@
+package vn.fis.finaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTestApplication.class, args);
+	}
+
+}
